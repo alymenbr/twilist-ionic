5066bb952f76775572fcd2d44b1f6bb38b64ca8f
Alysson Anthony <alymenbr@gmail.com>
2015-10-27 01:49:31 -0200

    madrugada

--- www/js/app.js
+++ www/js/app.js
@@ -5,15 +5,19 @@
 // the 2nd parameter is an array of 'requires'
 angular.module('starter', ['ionic', 'ngCordovaOauth', 'ngTwitter'])
 
+.config(function($ionicConfigProvider) {
+    $ionicConfigProvider.tabs.position('top');
+})
+
 .run(function($ionicPlatform) {
   $ionicPlatform.ready(function() {
     // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
     // for form inputs)
-/*
+
     if(window.cordova && window.cordova.plugins.Keyboard) {
       cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
     }
-*/
+
     if(window.StatusBar) {
       StatusBar.styleDefault();
     }
@@ -32,6 +36,7 @@ angular.module('starter', ['ionic', 'ngCordovaOauth', 'ngTwitter'])
   var myToken = '';
   $scope.tweet = {};
   $scope.userLists = [];
+  $scope.tweets = [];
 
   $ionicPlatform.ready(function() {
     myToken = JSON.parse(window.localStorage.getItem(twitterKey));
@@ -40,7 +45,6 @@ angular.module('starter', ['ionic', 'ngCordovaOauth', 'ngTwitter'])
         myToken = succ;
         window.localStorage.setItem(twitterKey, JSON.stringify(succ));
         $twitterApi.configure(clientId, clientSecret, succ);
-
         $scope.getUserLists();
       }, function(error) {
         console.log(error);
@@ -51,39 +55,24 @@ angular.module('starter', ['ionic', 'ngCordovaOauth', 'ngTwitter'])
     }
   });
 
-  $scope.showHomeTimeline = function() {
-    $twitterApi.getHomeTimeline().then(function(data) {
-      $scope.home_timeline = data;
-    });
-  };
-
-  $scope.submitTweet = function() {
-    $twitterApi.postStatusUpdate($scope.tweet.message).then(function(result) {
-      $scope.showHomeTimeline();
-    });
-  }
-
   $scope.getUserLists = function() {
     $twitterApi.getRequest(USER_LISTS_URL, {screen_name: myToken.screen_name}).then(function(data) {
       for (var i = 0; i < data.lists.length; i++) {
         var newList = {id: data.lists[i].id, name: data.lists[i].name, tweets: []};
         $scope.userLists.push(newList);
-        $scope.getListTweets(newList.id);
       }
     });
   }
 
-  $scope.getListTweets = function(listId) {
-    $twitterApi.getRequest(LIST_FEED_URL, {list_id: listId, include_entities: true, count: 10}).then(function(data) {
-      for (var i = 0; i < $scope.userLists.length; i++) {
-        if($scope.userLists[i].id === listId)
-          $scope.userLists[i].tweets = data;
-      }
+  $scope.onTabSelected = function(listId) {
+    $twitterApi.getRequest(LIST_FEED_URL, {list_id: listId, include_entities: true, count: 5}).then(function(data) {
+          $scope.tweets = data;
+          $scope.$broadcast('scroll.refreshComplete');
     });
   }
 
   $scope.doRefresh = function() {
-    $scope.showHomeTimeline();
+    // $scope.showHomeTimeline();
     $scope.$broadcast('scroll.refreshComplete');
   };
 
